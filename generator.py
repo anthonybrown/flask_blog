1b0eeddc1b963a520695a0cbb39654e2e6c34cf3
Tony Brown <tony@tonybrownsdesign.com>
2013-11-19 06:26:03 -0500

    add Atom Feed

--- generator.py
+++ generator.py
@@ -3,8 +3,9 @@
 import os
 import collections
 
-from flask import Flask, render_template, url_for, abort
+from flask import Flask, render_template, url_for, abort, request
 from werkzeug import cached_property
+from werkzeug.contrib.atom import AtomFeed
 import markdown
 import yaml
 
@@ -95,9 +96,9 @@ class Post(object):
             content = fin.read().split('\n\n', 1)[1].strip()
         return markdown.markdown(content)
 
-    @property
-    def url(self):
-        return url_for('post', path=self.urlpath)
+    #@property
+    def url(self, _external=False):
+        return url_for('post', path=self.urlpath, _external=_external)
 
     def _initialize_metadata(self):
         content = ''
@@ -127,6 +128,24 @@ def post(path):
     #import ipdb; ipdb.set_trace()
     post = blog.get_post_or_404(path)
     return render_template('post.html', post=post)
+
+@app.route('/feed.atom')
+def feed():
+    """setting up an Atom Feed"""
+    feed = AtomFeed('Recent Articles',
+                    feed_url=request.url,
+                    url=request.url_root)
+    posts = blog.posts[:10]
+    title = lambda p: '%s: %s' % (p.title, p.subtitle) if hasattr(p, 'subtitle') else p.title
+    for post in posts:
+        feed.add(title(post),
+                unicode(post.html),
+                content_type='html',
+                author='Tony Brown',
+                url=post.url(_external=True),
+                updated=post.date,
+                published=post.date)
+    return feed.get_response()
 
 
 if __name__ == '__main__':
